6f075d47815bfd7a90157782ece560b6bdced690
tgen0123 <100363182+tgen0123@users.noreply.github.com>
2024-09-01 23:37:42 +0700

    Update web.js

--- server/src/routes/web.js
+++ server/src/routes/web.js
@@ -14,7 +14,7 @@ let initWebRoutes = (app) => {
   route.get('/userinfor', Acount.authenticateJWT, Acount.getUserInfor);
   route.get('/productdetail/:productID', Products.getProductDetail);
   route.get('/allproducts', Products.getAllProducts);
-  route.get('/admin', Acount.getAllAccounts);
+  route.get('/admin', Acount.authenticateJWT, Acount.getAllAccounts);
   route.post('/login', Acount.login);
 
 
